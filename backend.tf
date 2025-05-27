f01c1ecf74396746de843cece316f5c430f125e4
Sneha <snehabopche1@gmail.com>
2025-05-27 16:21:24 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tf-backend-rg"
-    storage_account_name = "tfbackendstorage1234"
+    storage_account_name = "tfbackendstorage12345"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
