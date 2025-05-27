1023e8f1f0ea3e03e65037af985ca814faa9455d
Sneha <snehabopche1@gmail.com>
2025-05-27 16:23:00 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,41 +7,40 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+# Azure VM
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
+  name                = "myVnet"
   address_space       = ["10.0.0.0/16"]
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = var.subnet_name
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = var.vnet_name
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "${var.vm_name}-nic"
+  name                = "myNIC"
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "internal"
+    name                          = "myIPConfig"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                  = var.vm_name
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                  = "myVM"
+  resource_group_name   = azurerm_resource_group.rg.name
   location              = var.location
-  resource_group_name   = var.resource_group_name
   size                  = "Standard_B1s"
-  admin_username        = var.admin_username
-  admin_password        = var.admin_password
-  disable_password_authentication = false
-
+  admin_username        = "azureuser"
+  admin_password        = "Password1234!"
   network_interface_ids = [azurerm_network_interface.nic.id]
 
   os_disk {
@@ -50,40 +49,34 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
     version   = "latest"
   }
 }
 
+# Blob Storage
 resource "azurerm_storage_account" "storage" {
-  name                     = var.storage_account_name
-  resource_group_name      = var.resource_group_name
+  name                     = "myblobstorexyz"
+  resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_container" "container" {
-  name                  = var.container_name
-  storage_account_name  = var.storage_account_name
-  container_access_type = "private"
-}
-
-resource "azurerm_mssql_server" "sql_server" {
-  name                         = var.sql_server_name
-  resource_group_name          = var.resource_group_name
+# SQL Server and Database
+resource "azurerm_mssql_server" "sqlserver" {
+  name                         = "mysqlserverxyz"
+  resource_group_name          = azurerm_resource_group.rg.name
   location                     = var.location
   version                      = "12.0"
-  administrator_login          = var.sql_admin_user
-  administrator_login_password = var.sql_admin_password
+  administrator_login          = "sqladminuser"
+  administrator_login_password = "SqlPassword1234!"
 }
 
 resource "azurerm_mssql_database" "sqldb" {
-  name                = var.sql_db_name
-  resource_group_name = var.resource_group_name
-  server_id           = azurerm_mssql_server.sql_server.id
-  sku_name            = "Basic"
+  name           = "mydatabase"
+  server_id      = azurerm_mssql_server.sqlserver.id
+  sku_name       = "Basic"
 }
-
