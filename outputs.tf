faaf82eb8ca8d83b554e1cb439a7046193500cea
Sneha <snehabopche1@gmail.com>
2025-05-27 16:27:30 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,3 @@
-output "resource_group_name" {
-  description = "Name of the resource group"
-  value       = azurerm_resource_group.rg.name
-}
-
-output "virtual_network_name" {
-  description = "Name of the virtual network"
-  value       = azurerm_virtual_network.vnet.name
-}
-
-output "subnet_name" {
-  description = "Name of the subnet"
-  value       = azurerm_subnet.subnet.name
-}
-
-output "network_interface_id" {
-  description = "ID of the network interface"
-  value       = azurerm_network_interface.nic.id
-}
-
 output "vm_name" {
   description = "Name of the virtual machine"
   value       = azurerm_linux_virtual_machine.vm.name
